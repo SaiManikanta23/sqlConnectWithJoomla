359804d3e1460b5c71ce003010f61274ff9e1f53
SaiManikanta23 <infomanikanta@gmail.com>
2016-01-29 16:12:25 -0600

    Create InnerDatabase.php

---
+++ InnerDatabase.php
@@ -0,0 +1,17 @@
+<div><h1>Calling the database that we are using presently </h1></div>
+<?php
+<!-- get a database connection -->
+$db = JFactory::getDbo();
+
+<!--Query Object-->
+$query = $db->getQuery(true);
+
+<!--Make column selection; replace column_name with required column & dbName with database-->
+$query->select($db->quoteName('Column_Name'));
+$query->from($db->quoteName('dbName'));
+
+<!--Result set and printing results-->
+$db->setQuery($query);
+$result = $db->loadRow();
+print_r($result);
+
