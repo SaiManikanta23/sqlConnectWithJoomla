d2f00fccd38e74c473d8e0c534bf47be4d83053a
SaiManikanta23 <infomanikanta@gmail.com>
2016-01-29 16:28:24 -0600

    Update InnerDatabase.php

--- InnerDatabase.php
+++ InnerDatabase.php
@@ -1,16 +1,16 @@
 <div><h1>Calling the database that we are using presently </h1></div>
 <?php
-<!-- get a database connection -->
-$db = JFactory::getDbo();
 
-<!--Query Object-->
+$db = JFactory::getDbo();   //get a database connection
+
+//Query Object
 $query = $db->getQuery(true);
 
-<!--Make column selection; replace column_name with required column & dbName with database-->
+//Make column selection; replace column_name with required column & dbName with database
 $query->select($db->quoteName('Column_Name'));
 $query->from($db->quoteName('dbName'));
 
-<!--Result set and printing results-->
+//Result set and printing results
 $db->setQuery($query);
 $result = $db->loadRow();
 print_r($result);
