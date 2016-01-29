e4030e1fc3446bbfe66067a31d5c346bb80f8847
SaiManikanta23 <infomanikanta@gmail.com>
2016-01-29 16:23:59 -0600

    Update ExternalDatabase.php

--- ExternalDatabase.php
+++ ExternalDatabase.php
@@ -1,2 +1,21 @@
 
 <div>External Database</div>
+
+<?php
+
+$option = array();
+$option['driver']='mysql';
+$option['host']='localhost';
+$option['user']='dbUsername';
+$option['password']='dbPass_word';
+$option['database']='Database_Name';
+$option['prefix']='prefix_Name';
+$db = JDatabaseDriver::getInstance($option);
+$query = $db->getQuery(true);
+$query->select($db->quoteName('Column_Name'));
+$query->from($db->quoteName('Table_Name'));
+$db->setQuery($query);
+$result = $db->loadRow();
+print_r($result);
+
+?>
